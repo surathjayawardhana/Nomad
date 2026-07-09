0a23cfcc9a207b6bfc3e65445509084ac981e86a
Surath Jayawardhana <surathjayawardhana@gmail.com>
2026-07-09 13:47:06 +0530

    update

--- star.py
+++ star.py
@@ -1,9 +1,28 @@
-for i in range(5):
-    print("*" * (i + 1))
+c = 1
+x = 5
+for i in range (5):
+    for j in range(x):
+        print("*" * c)
+        c= c+1
+    c= 1
+    x = x-1
+    print("\n")
 
-input("Press Enter to exit...")
+input()
 
-for i in range(5):
-    print(" " * (4 - i) + "*" * (i + 1))
+c = 1
+for i in range (1):
+    for j in range(5):
+        print("*" * c)
+        c= c+1
 
-input("Press Enter to exit...")
+
+input()
+
+n = 1
+for i in range (1):
+    for j in range(10):
+        print("*" * (n*n))
+        n+=1
+
+input()
